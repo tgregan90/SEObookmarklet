71b8a872a3025b7dddd98f7746fe6661004f9995
Tom Gregan <tgregan90@hotmail.co.uk>
2021-02-19 12:19:51 +0000

    Changed from switch to if's

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 (function(){
     const currentURL = window.location.href;
-    switch (currentURL) {
-        case currentURL.indexOf("https://www.google") && currentURL.indexOf("/search?q=") :
-            console.log("In a Google SERP")
-            break;
-
-        default: 
-        console.log("any other page")
-            break;
+    
+    if (currentURL.indexOf("://www.google") && currentURL.indexOf("/search") && currentURL.indexOf("?") && currentURL.indexOf("q=")){
+        console.log("In a Google SERP");
+    } else {
+        console.log("Any other site");
     }
+
 })();
