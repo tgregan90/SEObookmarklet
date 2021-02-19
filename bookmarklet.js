c4abeec2f29bc94bfd093e75b6a50ad967fc2c1c
Tom Gregan <tgregan90@hotmail.co.uk>
2021-02-19 12:46:04 +0000

    Changed to .then

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 function run(){
-    let code =  fetch("https://cdn.jsdelivr.net/gh/tgregan90/SEObookmarklet@latest/app.min.js");
-    code();
+    let code =  fetch("https://cdn.jsdelivr.net/gh/tgregan90/SEObookmarklet@latest/app.min.js")
+    .then(response=> response();)
 }
 run();
