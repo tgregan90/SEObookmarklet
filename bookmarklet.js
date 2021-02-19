53108b006dfc0913bfe76ec371ba28f19b439aff
Tom Gregan <tgregan90@hotmail.co.uk>
2021-02-19 14:52:45 +0000

    Changed bookmarklet to much simpler script block

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,3 @@
-async function run(){
-    let data = await fetch("https://cdn.jsdelivr.net/gh/tgregan90/SEObookmarklet@latest/app.js");
-    if(data.ok){
-        data();
-    }
-}   
+let s =document.createElement("script");
+s.src="https://cdn.jsdelivr.net/gh/tgregan90/SEObookmarklet@latest/app.js";
+document.body.insertAdjacentElement("afterbegin",s);
