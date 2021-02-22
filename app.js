08257589d55c0e2901fd1a881c7557d4c18cfd9b
Tom Gregan <tgregan90@hotmail.co.uk>
2021-02-22 09:41:31 +0000

    Adjusted to dynamically build script block

--- app.js
+++ app.js
@@ -3,13 +3,9 @@
     
     if (currentURL.indexOf("://www.google") && currentURL.indexOf("/search") && currentURL.indexOf("?") && currentURL.indexOf("q=")){
         console.log("In a Google SERP");
-        let data = await fetch("https://cdn.jsdelivr.net/gh/tgregan90/SEObookmarklet@latest/SERPs.min.js");
-        if(data.ok){
-            data();
-        }
-        
-    } else {
-        console.log("Any other site");
+        let s = document.createElement("script");
+        s.src="https://cdn.jsdelivr.net/gh/tgregan90/SEObookmarklet@latest/SERPs.min.js";
+        document.body.insertAdjacentElement("afterbegin",s);
     }
 
 })();
